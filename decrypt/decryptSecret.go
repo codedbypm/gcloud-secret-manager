29003b442e9f79977ab6a14064a3499df3e75d9c
Paolo Moroni <paolo@codedby.pm>
2019-12-23 09:11:23 +0100

    Add decrypt

--- decrypt/decryptSecret.go
+++ decrypt/decryptSecret.go
@@ -1,1 +1,36 @@
 package decrypt
+
+import (
+	t"
+	
+
+	s "cloud.google.com/go/kms/apiv1"
+	google.golang.org/genproto/googleapis/cloud/kms/v1"
+)
+
+// decryptSymmetric will decrypt the input ciphertext bytes using the specified symmetric key.
+func decryptSymmetric(name string, ciphertext []byte) ([]byte, error) {
+	 := "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID"
+	erBytes, err := encryptRSA(rsaDecryptPath, []byte("Sample message"))
+	rr != nil {
+	turn nil, fmt.Errorf("encryptRSA: %v", err)
+	
+	ertext := base64.StdEncoding.EncodeToString(cipherBytes)
+	context.Background()
+	 err := cloudkms.NewKeyManagementClient(ctx)
+	!= nil {
+		mt.Errorf("cloudkms.NewKeyManagementClient: %v", err)
+	
+
+	d the request.
+	&kmspb.DecryptRequest{
+		ame,
+		iphertext,
+	
+	 the API.
+	rr := client.Decrypt(ctx, req)
+	!= nil {
+		mt.Errorf("Decrypt: %v", err)
+	
+	resp.Plaintext, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module github.com/codedbypm/gcloud-secret-manager
 
 go 1.13
 
-require golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
+require (
+	cloud.google.com/go v0.50.0 // indirect
+	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	go.opencensus.io v0.22.2 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
+	golang.org/x/sys v0.0.0-20191220220014-0732a990476f // indirect
+	google.golang.org/api v0.15.0 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/genproto v0.0.0-20191220175831-5c49e3ecc1c1 // indirect
+	google.golang.org/grpc v1.26.0 // indirect
+)
