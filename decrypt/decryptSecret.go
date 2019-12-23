fbcf235b2f0f8c65614b346ecb740b00fb8e73f9
Paolo Moroni <paolo@codedby.pm>
2019-12-23 09:19:38 +0100

    Rename function

--- decrypt/decryptSecret.go
+++ decrypt/decryptSecret.go
@@ -1,36 +1,36 @@
 package decrypt
 
 import (
-	t"
-	
+	"context"
+	"fmt"
 
-	s "cloud.google.com/go/kms/apiv1"
-	google.golang.org/genproto/googleapis/cloud/kms/v1"
+	cloudkms "cloud.google.com/go/kms/apiv1"
+	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
 )
 
-// decryptSymmetric will decrypt the input ciphertext bytes using the specified symmetric key.
-func decryptSymmetric(name string, ciphertext []byte) ([]byte, error) {
-	 := "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID"
-	erBytes, err := encryptRSA(rsaDecryptPath, []byte("Sample message"))
-	rr != nil {
-	turn nil, fmt.Errorf("encryptRSA: %v", err)
-	
-	ertext := base64.StdEncoding.EncodeToString(cipherBytes)
-	context.Background()
-	 err := cloudkms.NewKeyManagementClient(ctx)
-	!= nil {
-		mt.Errorf("cloudkms.NewKeyManagementClient: %v", err)
-	
+// DecryptSymmetric will decrypt the input ciphertext bytes using the specified symmetric key.
+func DecryptSymmetric(name string, ciphertext []byte) ([]byte, error) {
+	// name := "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID"
+	// cipherBytes, err := encryptRSA(rsaDecryptPath, []byte("Sample message"))
+	// if err != nil {
+	//   return nil, fmt.Errorf("encryptRSA: %v", err)
+	// }
+	// ciphertext := base64.StdEncoding.EncodeToString(cipherBytes)
+	ctx := context.Background()
+	client, err := cloudkms.NewKeyManagementClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("cloudkms.NewKeyManagementClient: %v", err)
+	}
 
-	d the request.
-	&kmspb.DecryptRequest{
-		ame,
-		iphertext,
-	
-	 the API.
-	rr := client.Decrypt(ctx, req)
-	!= nil {
-		mt.Errorf("Decrypt: %v", err)
-	
-	resp.Plaintext, nil
+	// Build the request.
+	req := &kmspb.DecryptRequest{
+		Name:       name,
+		Ciphertext: ciphertext,
+	}
+	// Call the API.
+	resp, err := client.Decrypt(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("Decrypt: %v", err)
+	}
+	return resp.Plaintext, nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/codedbypm/gcloud-secret-manager
 go 1.13
 
 require (
-	cloud.google.com/go v0.50.0 // indirect
+	cloud.google.com/go v0.50.0
 	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
 	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
@@ -14,6 +14,6 @@ require (
 	golang.org/x/sys v0.0.0-20191220220014-0732a990476f // indirect
 	google.golang.org/api v0.15.0 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
-	google.golang.org/genproto v0.0.0-20191220175831-5c49e3ecc1c1 // indirect
+	google.golang.org/genproto v0.0.0-20191220175831-5c49e3ecc1c1
 	google.golang.org/grpc v1.26.0 // indirect
 )
